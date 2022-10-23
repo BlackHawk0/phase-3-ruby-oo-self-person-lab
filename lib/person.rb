fd707b66c7b4002c3d51e1b68d45abe96cadde9a
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-23 13:47:23 +0300

    instance methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,77 @@
 # your code goes here
+class Person
+    attr_reader :name, :hygiene, :happiness
+    attr_accessor  :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = value
+        # if happiness is greater than 10
+        @happiness = 10 unless @happiness < 10
+        # if happiness is less than zero
+        @happiness = 0 unless @happiness > 0
+    end
+
+    def hygiene=(new_hygiene)
+        @hygiene = new_hygiene
+        # if hygiene is greater than 10
+        @hygiene = 10 unless @hygiene < 10
+        # if hygiene is less than zero
+        @hygiene = 0 unless @hygiene > 0
+    end
+    def clean?
+        @hygiene > 7
+    end
+    def happy?
+        @happiness > 7
+    end
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+    def take_bath
+        self.hygiene = @hygiene + 4
+        hygiene
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out
+        self.happiness = @happiness += 2
+        self.hygiene = @hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        @friend = friend
+        @topic = topic
+        if topic == "politics" do
+            self.happiness -=2
+            friend.happiness -=2
+            "blah blah partisan blah lobbyist"
+        elsif topic == 'weather' do
+            
+        end
+
+end
+
+blake = Person.new('Blake')
+
+blake.happiness=55
+puts blake.happiness
+puts blake.hygiene
+# blake.hygiene=20
+puts blake.hygiene
+
+puts blake.clean?
+
+
